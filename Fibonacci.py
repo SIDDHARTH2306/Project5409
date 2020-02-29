e0930c7e626fd2bdc89c2a1e628ed2785c02f192
Siddharth Anikar <siddharth.anikar@dal.ca>
2020-02-28 20:30:30 -0400

    Commit Fibo

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,7 @@
 import time
 import random
+import pandas as pd
+import csv
 
 file = open("input.txt","r")
 
@@ -21,7 +23,7 @@ for i in line:
         print(n1)
     else:
         #print("Fibonacci sequence:")
-        start = time.time()
+        start = time.time() * 1000
         while count < N:
            #print(n1)
            nth = n1 + n2
@@ -29,7 +31,10 @@ for i in line:
            n1 = n2
            n2 = nth
            count += 1
-        end = time.time()
+        end = time.time() * 1000
         t = end-start
         print("Request ID: ",random.randint(1,100),"Time: ",t,"N: ",N)
         timelist.append(t)
+
+df = pd.DataFrame({'Request Time':timelist})
+df.to_csv('Request Time.csv',index=False)
